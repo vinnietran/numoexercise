c1ae2ed19dd0b4d3cc27b104129f53dcb1b8f8ac
VinTran <tranquillo.vinnie@gmail.com>
2019-11-01 13:08:27 -0400

    Moving to componenet

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import "./App.css";
 import "./components/layout/NewActivity";
 import NewActivity from "./components/layout/NewActivity";
 import DisplayActivity from "./components/layout/DisplayActivity";
-import axios from 'axios';
+import axios from "axios";
 
 class App extends Component {
   render() {
+    const { activityName } = this.state;
     return (
       <div className="App">
         <h1>Hello</h1>
@@ -14,27 +15,35 @@ class App extends Component {
           <div className="row">
             <div className="col-md-12">
               <DisplayActivity />
+              <h1>{activityName}</h1>
+              <NewActivity />
             </div>
           </div>
           <br></br>
           <br></br>
           <br></br>
-          <NewActivity />
         </div>
       </div>
     );
   }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      activityName: ""
+    };
+  }
+
   componentDidMount() {
-    axios.get('http://www.boredapi.com/api/activity/')
-    .then(response => {
-      console.log(response.data);
-    })
-    .catch(error => {
-      console.log(error);
-    });
+    axios
+      .get("http://www.boredapi.com/api/activity/")
+      .then(response => {
+        this.setState({ activityName: response.data.activity });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 }
-
-
 
 export default App;
